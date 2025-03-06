478f41894f4784acc2cf0196ee867e44077a2da3
rhavymaia <rhavy.guedes@ifpb.edu.br>
2025-03-06 09:34:28 -0300

    Listagem de clientes.

---
+++ src/dataset.js
@@ -0,0 +1,22 @@
+let clientesDataset = [
+  {
+    id: '1',
+    nome: 'José da Silva',
+    email: 'js@mail.com',
+    cpf: '738.153.640-43',
+  },
+  {
+    id: '2',
+    nome: 'Maria Alencar',
+    email: 'ma@mail.com',
+    cpf: '639.589.750-03',
+  },
+];
+
+let obrasDataset = [
+  {
+    //...
+  },
+];
+
+export { clientesDataset, obrasDataset };
--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
-let dataset = [
-  {
-    nome: 'José da Silva',
-    email: 'js@mail.com',
-    cpf: '738.153.640-43',
-  },
-  {
-    nome: 'Maria Alencar',
-    email: 'ma@mail.com',
-    cpf: '639.589.750-03',
-  },
-];
+import { clientesDataset } from './dataset.js';
+
+function loadLocalStorage() {
+  let clientesLocalStorage = localStorage.getItem('clientes');
+  if (clientesLocalStorage == null) {
+    localStorage.setItem('clientes', JSON.stringify(clientesDataset));
+    clientesLocalStorage = localStorage.getItem('clientes');
+  }
+  return JSON.parse(clientesLocalStorage);
+}
 
 let clientesTable = document.getElementById('clientesTable');
 
-for (const cliente of dataset) {
+for (const cliente of loadLocalStorage()) {
   clientesTable.insertAdjacentHTML(
     'beforeend',
     `<tr>
-      <td></td>
+      <td>${cliente.id}</td>
       <td>${cliente.nome}</td>
       <td>${cliente.email}</td>
       <td>${cliente.cpf}</td>
     </tr>`,
   );
 }
+
+let clienteForm = document.getElementById('clienteForm');
+clienteForm.onsubmit = (event) => {
+  event.preventDefault();
+  console.log('Submeteu o formulário');
+};
